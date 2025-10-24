6176e8f3c4f20186c2a29f8bcdc29a7376b3f499
shahd sofy <shahdsofy12@gamil.com>
2025-10-25 00:25:45 +0300

    Add project files.

---
+++ Arabeya.APIs.Controllers/Controllers/Base/BaseController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Arabeya.APIs.Controllers.Controllers.Base
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BaseController : ControllerBase
+    {
+        
+    }
+}
