ba61b96fbe93e25cac2b8a61d39a4e36382d7024
shahd sofy <shahdsofy12@gamil.com>
2025-11-14 00:47:49 +0200

    Update Account Controller

--- Arabeya.APIs.Controllers/Controllers/Account/AccountController.cs
+++ Arabeya.APIs.Controllers/Controllers/Account/AccountController.cs
@@ -12,37 +12,37 @@ namespace Arabeya.APIs.Controllers.Controllers.Account
 		public async Task<ActionResult<UserDto>> Login(LoginDto model)
 		{
 			var result = await serviceManager.authService.LoginAsync(model);
-			return Ok(result);
+			return StatusCode((int)result.StatusCode, result);
 		}
 
 		[HttpPost("Register")]
 		public async Task<ActionResult<UserDto>> Register(RegisterDto model)
 		{
 			var result = await serviceManager.authService.RegisterAsync(model);
-			return Ok(result);
+			return StatusCode((int)result.StatusCode, result);
 		}
 
-		[Authorize]
 		[HttpPost("Confirm Email")]
 		public async Task<ActionResult<string>> ConfirmEmail(ConfirmEmailDto model)
 		{
 			var result = await serviceManager.authService.ConfirmUserEmail(model);
-			return Ok(result);
+			return StatusCode((int)result.StatusCode,result);
 		}
 
 		[HttpPost("Forget Password")]
 		public async Task<ActionResult> ForgetPassword(string email)
 		{
 			var result = await serviceManager.authService.ForgetPasswordAsync(email);
-			return Ok(result);
+			return StatusCode((int)result.StatusCode, result);
 		}
 
-		[Authorize]
+
+
 		[HttpPost("Reset Password")]
 		public async Task<ActionResult> ResetPassword(ResetPasswordDto model)
 		{
 			var result = await serviceManager.authService.ResetPasswordAsync(model);
-			return Ok(result);
+			return StatusCode((int)result.StatusCode, result);
 		}
 	}
 }
